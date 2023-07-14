c335b5cec16d7dbb39496e48e5673eb469c3eb20
Lucas do Nascimento Silva <67718114+Lucas-ns@users.noreply.github.com>
2023-07-14 15:57:51 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <>
+        <h1>Oi Mô,<br/>Só passando pra lembrar que eu amo você.<br/>Ass.: Mozão &#128150;</h1>
+        <p><strong>PS: Me manda uma selfie se você leu.</strong> &#128536;</p>
+    </>
+}
+
+export default Home;
